09b7d47a9e5ab025215f6dcb1fe6f2d09cf2ec26
TeleginD <dtelegin.spok@yandex.ru>
2016-07-26 08:35:27 +0300

    Delete company

--- company1/.left.menu.php
+++ company1/.left.menu.php
@@ -1,37 +1,37 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $aMenuLinks = Array(
-	//Array(
-	//	"Компания", 
-	//	"company/", 
-	//	Array(), 
-	//	Array(), 
-	//	"" 
-	//),
 	Array(
-		"Новости", 
-		"news/", 
+		"О компании", 
+		"./", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
-		"Продукция", 
-		"products/", 
+		"Руководство", 
+		"management.php", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
-		"Услуги", 
-		"services/", 
+		"Миссия и стратегия", 
+		"mission.php", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
-		"Контакты", 
-		"contacts/", 
+		"История", 
+		"history.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Вакансии", 
+		"vacancies.php", 
 		Array(), 
 		Array(), 
 		"" 
